565d37a7258ba0a526343af124bd959bc787c1d7
Buntcz <testtigeryt@gmail.com>
2024-05-01 08:35:32 +0300

    --all

--- script.js
+++ script.js
@@ -59,52 +59,53 @@ class Tree {
     }
    }
    delete(data) {
-    let current = this.root
-    if(data < current.data && current.left) {
-        current.left = current.left.delete(data);
-    } else if(data > current.data && current.right) {
-        current.right = current.right.delete(data);
-    } else {
-        if(data === current.data) {
-            if(current.right && current.left) {
-                let minVal = current.right.findMin();
-                current.data = minVal;
-                current.right = current.right.delete(minVal)
-            }
-            else if(current.left) {
-                return current.left;
-            } else if(current.right) {
-                return current.right
-            } else {
-                return null;
-            }
-        }
-        return current
-    }
+    this.root = this.deleteNode(this.root, data);
 }
 
-findMin() {
-    let current = this.root
-    if(current.left) {
-        return current.left.findMin();
-    } else {
-        return current.data;
+deleteNode(root, data) {
+    if(root === null) {
+        return root
     }
+    if(data < root.data) {
+        root.left = this.deleteNode(root.left, data)
+    } else if(data > root.data) {
+        root.right = this.deleteNode(root.right, data);
+    } else {
+        if(!root.right && !root.left) {
+            return null;
+        }
+        if(!root.left) {
+            return root.right;
+        } else if(!root.right) {
+            root.left;
+        }
+        root.data = this.findMin(root.right)
+        root.right = this.deleteNode(root.right, root.data);
+    }
+    return root;
+}
+
+findMin(root) {
+   if(!root.left) {
+    return root.data;
+   } else {
+    return this.findMin(root.left)
+   }
 }
 
    find(data) {
     let current = this.root;
     while(current) {
         if(data === current.data) {
-            return data;
-        }
+            return true;
+        } 
         if(data < current.data) {
             current = current.left;
         } else if ( data > current.data) {
             current = current.right;
         }
     }
-    return current;
+   
    }
   levelOrder(callback) {
     let queue = [];
@@ -126,16 +127,97 @@ findMin() {
 
         callback(nextNode.data);
     }
-
   }
+
+  inOrder(callback) {
+    const traverse = (node) => {
+        if(node) {
+            traverse(node.left);
+            callback(node);
+            traverse(node.right);
+        }
+    };
+    traverse(this.root);
+  }
+
+  preOrder(callback) {
+    const traverse = (node) => {
+        if(node) {
+            callback(node);
+            traverse(node.left);
+            traverse(node.right);
+        }
+    };
+    traverse(this.root);
+  }
+  
+postOrder(callback) {
+    const traverse = (node) => {
+    if(node) {
+        traverse(node.left);
+        traverse(node.right);
+        callback(node)
+    }
+    };
+    traverse(this.root);
+}
+
+height(root) {
+    if(!root) {
+        return 0;
+    }
+    const leftHeight = this.height(root.left);
+    const rightHeight = this.height(root.right);
+    return Math.max(leftHeight, rightHeight) + 1
+}
+
+depth(node) {
+    if(!node) {
+        return 0;
+    }
+    const leftDepth = this.depth(node.left);
+    const rightDepth = this.depth(node.right);
+    return Math.max(leftDepth, rightDepth) + 1;
+}
+
+isBalanced(root) {
+  if(root === null) {
+     return true;
+  }
+  const leftHeight = this.height(root.left);
+  const rightHeight = this.height(root.right);
+  const heightDiff = Math.abs(leftHeight - rightHeight);
+  if(heightDiff > 1) {
+    return false
+  } else {
+    return true;
+  }
+}
+
+rebalance() {
+    if(this.isBalanced(this.root)) {
+        return;
+    }
+    let nodes = [];
+    this.inOrder((node) => nodes.push(node.data));
+
+   this.root = this.buildTree(nodes);
+}
+
 }
 
 const array = [1,7,4,23,8,9,4,3,5,7,8,67,6345,324];
 const treeTest = new Tree(array);
 
-console.log(treeTest.insert(18))
-treeTest.delete(67)
-console.log(treeTest.buildTree(array))
+
+const Node1 = treeTest.insert(19);
+const Node2 = treeTest.insert(2);
+const Node3 = treeTest.insert(7000);
+const Node4 = treeTest.insert(8000);
+const Node5 = treeTest.insert(9000);
+const Node6 = treeTest.insert(10000);
+treeTest.rebalance();
+console.log(treeTest.buildTree(array));
 
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -151,4 +233,9 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     }
   };
  
-  prettyPrint(treeTest.root)
+  treeTest.inOrder((node) => console.log(node.data))
+  prettyPrint(treeTest.root);
+console.log(treeTest.isBalanced(treeTest.root));
+  
+
+  
